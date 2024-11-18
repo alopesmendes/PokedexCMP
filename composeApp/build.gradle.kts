434354d14495bc67826997720f67e4fb7b99741e
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-11-18 09:26:14 +0100

    [#1] :heavy_plus_sign: add ktlint plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,143 +1,175 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+	alias(libs.plugins.kotlinMultiplatform)
+	alias(libs.plugins.androidApplication)
+	alias(libs.plugins.composeMultiplatform)
+	alias(libs.plugins.composeCompiler)
+	alias(libs.plugins.ktlint)
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-    
-    jvm("desktop")
-    
-    sourceSets {
-        val desktopMain by getting
-        
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+	androidTarget {
+		@OptIn(ExperimentalKotlinGradlePluginApi::class)
+		compilerOptions {
+			jvmTarget.set(JvmTarget.JVM_11)
+		}
+	}
 
-            // KOIN
-            implementation(libs.koin.android)
+	listOf(
+		iosX64(),
+		iosArm64(),
+		iosSimulatorArm64(),
+	).forEach { iosTarget ->
+		iosTarget.binaries.framework {
+			baseName = "ComposeApp"
+			isStatic = true
+		}
+	}
 
-            // KTOR
-            implementation(libs.ktor.client.okhttp)
-        }
+	jvm("desktop")
 
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
+	sourceSets {
+		val desktopMain by getting
 
-            // KOIN
-            implementation(project.dependencies.platform(libs.koin.bom))
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
+		androidMain.dependencies {
+			implementation(compose.preview)
+			implementation(libs.androidx.activity.compose)
 
-            // KTOR
-            implementation(libs.bundles.ktor)
-        }
+			// KOIN
+			implementation(libs.koin.android)
 
-        iosMain.dependencies {
-            // KTOR
-            implementation(libs.ktor.client.darwin)
-        }
+			// KTOR
+			implementation(libs.ktor.client.okhttp)
+		}
 
-        desktopMain.dependencies {
-            implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
+		commonMain.dependencies {
+			implementation(compose.runtime)
+			implementation(compose.foundation)
+			implementation(compose.material3)
+			implementation(compose.ui)
+			implementation(compose.components.resources)
+			implementation(compose.components.uiToolingPreview)
+			implementation(libs.androidx.lifecycle.viewmodel)
+			implementation(libs.androidx.lifecycle.runtime.compose)
 
-            // KTOR
-            implementation(libs.ktor.client.cio)
-        }
-    }
+			// KOIN
+			implementation(project.dependencies.platform(libs.koin.bom))
+			implementation(libs.koin.core)
+			implementation(libs.koin.compose)
+			implementation(libs.koin.compose.viewmodel)
+
+			// KTOR
+			implementation(libs.bundles.ktor)
+		}
+
+		iosMain.dependencies {
+			// KTOR
+			implementation(libs.ktor.client.darwin)
+		}
+
+		desktopMain.dependencies {
+			implementation(compose.desktop.currentOs)
+			implementation(libs.kotlinx.coroutines.swing)
+
+			// KTOR
+			implementation(libs.ktor.client.cio)
+		}
+	}
 }
 
 android {
-    namespace = "org.ailtontech.pokedex"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+	namespace = "org.ailtontech.pokedex"
+	compileSdk = libs.versions.android.compileSdk.get().toInt()
 
-    defaultConfig {
-        applicationId = "org.ailtontech.pokedex"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+	defaultConfig {
+		applicationId = "org.ailtontech.pokedex"
+		minSdk = libs.versions.android.minSdk.get().toInt()
+		targetSdk = libs.versions.android.targetSdk.get().toInt()
+		versionCode = 1
+		versionName = "1.0"
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
+	buildTypes {
+		getByName("release") {
+			isMinifyEnabled = false
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_11
+		targetCompatibility = JavaVersion.VERSION_11
+	}
 }
 
 dependencies {
-    debugImplementation(compose.uiTooling)
+	debugImplementation(compose.uiTooling)
 }
 
 compose.desktop {
-    application {
-        mainClass = "org.ailtontech.pokedex.MainKt"
+	application {
+		mainClass = "org.ailtontech.pokedex.MainKt"
 
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "org.ailtontech.pokedex"
-            packageVersion = "1.0.0"
-        }
-    }
+		nativeDistributions {
+			targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+			packageName = "org.ailtontech.pokedex"
+			packageVersion = "1.0.0"
+		}
+	}
 }
 
 tasks.register<Copy>("copyPrepareCommitMsgHook") {
-    description = "Copy prepare-commit-msg git hook from the scripts to the .git/hooks folder."
-    group = "git hooks"
-    outputs.upToDateWhen { false }
-    from("$rootDir/scripts/prepare-commit-msg")
-    into("$rootDir/.git/hooks/")
-    doLast {
-        // Make the script executable
-        val preCommitHook = file("$rootDir/.git/hooks/prepare-commit-msg")
-        if (preCommitHook.exists()) {
-            preCommitHook.setExecutable(true)
-        }
-    }
+	description = "Copy prepare-commit-msg git hook from the scripts to the .git/hooks folder."
+	group = "git hooks"
+	outputs.upToDateWhen { false }
+	from("$rootDir/scripts/prepare-commit-msg")
+	into("$rootDir/.git/hooks/")
+	doLast {
+		// Make the script executable
+		val preCommitHook = file("$rootDir/.git/hooks/prepare-commit-msg")
+		if (preCommitHook.exists()) {
+			preCommitHook.setExecutable(true)
+		}
+	}
+}
+
+tasks.register<Copy>("copyPreCommitHook") {
+	description = "Copy pre-commit git hook from the scripts to the .git/hooks folder."
+	group = "git hooks"
+	outputs.upToDateWhen { false }
+	from("$rootDir/scripts/pre-commit")
+	into("$rootDir/.git/hooks/")
+	doLast {
+		// Make the script executable
+		val preCommitHook = file("$rootDir/.git/hooks/pre-commit")
+		if (preCommitHook.exists()) {
+			preCommitHook.setExecutable(true)
+		}
+	}
 }
 
 tasks.register("initializeProject") {
-    description = "Initialize the project."
-    dependsOn("copyPrepareCommitMsgHook")
+	description = "Initialize the project."
+	dependsOn("copyPrepareCommitMsgHook")
+	dependsOn("copyPreCommitHook")
+}
+
+ktlint {
+	android.set(false)
+	verbose.set(true)
+	outputToConsole.set(true)
+	outputColorName.set("RED")
+	enableExperimentalRules.set(true)
+
+	reporters {
+		reporter(ReporterType.HTML)
+		reporter(ReporterType.CHECKSTYLE)
+		reporter(ReporterType.JSON)
+	}
 }
