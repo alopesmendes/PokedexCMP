b66ae4b123b01602ffa6af946ba3ca5a2d9c01aa
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-11-22 20:20:31 +0100

    [#4] :art: update App to use windowSizeClass

--- composeApp/src/commonMain/kotlin/org/ailtontech/pokedex/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/org/ailtontech/pokedex/presentation/App.kt
@@ -2,40 +2,63 @@ package org.ailtontech.pokedex.presentation
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
 import org.ailtontech.pokedex.Greeting
+import org.ailtontech.pokedex.presentation.components.TextBody
+import org.ailtontech.pokedex.presentation.components.TextCaption
+import org.ailtontech.pokedex.presentation.components.TextTitle
 import org.ailtontech.pokedex.presentation.theme.PokedexTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import pokedex.composeapp.generated.resources.Res
 import pokedex.composeapp.generated.resources.compose_multiplatform
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 @Preview
 fun App() {
-	PokedexTheme {
-		var showContent by remember { mutableStateOf(false) }
-		Column(
-			Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
-			horizontalAlignment = Alignment.CenterHorizontally
-		) {
-			Button(onClick = { showContent = !showContent }) {
-				Text("Click me!")
+	BoxWithConstraints(
+		modifier = Modifier.fillMaxSize(),
+	) {
+		val dpSize =
+			remember(maxWidth, maxHeight) {
+				DpSize(maxWidth, maxHeight)
 			}
-			AnimatedVisibility(showContent) {
+		val windowSizeClass =
+			remember(dpSize) { WindowSizeClass.calculateFromSize(dpSize) }
+		PokedexTheme(
+			windowSizeClass = windowSizeClass,
+		) {
+			var showContent by remember { mutableStateOf(false) }
+			Column(
+				Modifier.fillMaxSize(),
+				horizontalAlignment = Alignment.CenterHorizontally,
+			) {
 				val greeting = remember { Greeting().greet() }
-				Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-					Image(painterResource(Res.drawable.compose_multiplatform), null)
-					Text("Compose: $greeting")
+
+				TextCaption(text = "Compose : $greeting")
+				TextBody(text = "Text body that should change the size")
+				Button(onClick = { showContent = !showContent }) {
+					TextTitle(
+						text = "Click me!",
+					)
+				}
+				AnimatedVisibility(showContent) {
+					Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+						Image(painterResource(Res.drawable.compose_multiplatform), null)
+						Text("Compose: $greeting")
+					}
 				}
 			}
 		}
