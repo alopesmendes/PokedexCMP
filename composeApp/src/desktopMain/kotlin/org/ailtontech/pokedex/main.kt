37c88e00e598ee03742a11eaab9c950f5016ceff
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-11-24 12:09:29 +0100

    [#4] :adhesive_bandage: import app

--- composeApp/src/desktopMain/kotlin/org/ailtontech/pokedex/main.kt
+++ composeApp/src/desktopMain/kotlin/org/ailtontech/pokedex/main.kt
@@ -2,12 +2,14 @@ package org.ailtontech.pokedex
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.ailtontech.pokedex.presentation.App
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Pokedex",
-    ) {
-        App()
-    }
-}
+fun main() =
+	application {
+		Window(
+			onCloseRequest = ::exitApplication,
+			title = "Pokedex",
+		) {
+			App()
+		}
+	}
