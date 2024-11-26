00ef317cf307cb700101529653ccb8af4db72ca7
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-11-26 17:35:20 +0100

    [#9] :art: add pokemon view model

---
+++ composeApp/src/commonMain/kotlin/org/ailtontech/pokedex/features/pokemon/presentation/viewModels/PokemonViewModel.kt
@@ -0,0 +1,22 @@
+package org.ailtontech.pokedex.features.pokemon.presentation.viewModels
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.ailtontech.pokedex.core.utils.BaseViewModel
+import org.ailtontech.pokedex.features.pokemon.presentation.reducers.PokemonEffect
+import org.ailtontech.pokedex.features.pokemon.presentation.reducers.PokemonEvent
+import org.ailtontech.pokedex.features.pokemon.presentation.reducers.PokemonReducer
+import org.ailtontech.pokedex.features.pokemon.presentation.reducers.PokemonState
+
+class PokemonViewModel(
+	reducer: PokemonReducer,
+) : BaseViewModel<PokemonState, PokemonEvent, PokemonEffect>(
+		initialState = PokemonState(),
+		reducer = reducer,
+	) {
+	override fun loadData() {
+		viewModelScope.launch {
+			sendEvent(PokemonEvent.GetPokemonList())
+		}
+	}
+}
